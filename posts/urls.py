08cee4fa85ef7458cd79f4cec8797b88f0182c74
Michael-Kratz <Michael.Kratz@sdgku.edu>
2025-10-31 01:17:42 -0700

    113 2nd assignment

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from .views import PostListView, PostDetailView
 
 urlpatterns = [
-
-    
+    path("", PostListView.as_view(), name="post"),
+    path("detail/<int:pk>/", PostDetailView.as_view(), name="post_detail"),  
 ]
