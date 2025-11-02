0b4b781aa3169f6779cb2ffa718de445d1b21104
Michael-Kratz <Michael.Kratz@sdgku.edu>
2025-11-02 02:24:59 -0800

    113 final assignment

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, DraftPostListView, ArchivedPostListView
 
 urlpatterns = [
     path("", PostListView.as_view(), name="post"),
@@ -7,4 +7,6 @@ urlpatterns = [
     path("new/", PostCreateView.as_view(), name="post_create"),
     path("edit/<int:pk>/", PostUpdateView.as_view(), name="post_edit"),
     path("delete/<int:pk>/", PostDeleteView.as_view(), name="post_delete"),
+    path("arcived/", ArchivedPostListView.as_view(), name="archived_list"),
+    path("draft/", DraftPostListView.as_view(), name="draft_list"),
 ]
